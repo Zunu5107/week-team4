d5413241cd277e0b4ae6ec40ea79b083ddd68b95
kki4504 <rhkddlf9876@gmail.com>
2023-07-21 16:50:07 +0900

    feat: timestamped

--- src/main/java/com/clone/team4/Team4Application.java
+++ src/main/java/com/clone/team4/Team4Application.java
@@ -2,8 +2,10 @@ package com.clone.team4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Team4Application {
 
     public static void main(String[] args) {
