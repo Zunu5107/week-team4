2bf7a3abbea514f42cbf26a38138b869562da713
Zunu5107 <cosoob5107@gmail.com>
2023-07-22 09:34:25 +0900

    init mypage

---
+++ src/main/java/com/clone/team4/domain/mypage/controller/MyPageController.java
@@ -0,0 +1,12 @@
+package com.clone.team4.domain.mypage.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class MyPageController {
+
+}
