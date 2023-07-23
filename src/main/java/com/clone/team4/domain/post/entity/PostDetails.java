14405ea95bdfeee25a0068b4b87f58c5e63ba4e2
Zunu5107 <68607939+Zunu5107@users.noreply.github.com>
2023-07-23 16:12:08 +0900

    Update PostDetails.java

--- src/main/java/com/clone/team4/domain/post/entity/PostDetails.java
+++ src/main/java/com/clone/team4/domain/post/entity/PostDetails.java
@@ -2,16 +2,8 @@ package com.clone.team4.domain.post.entity;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import com.clone.team4.domain.post.dto.PostRequestDto;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
