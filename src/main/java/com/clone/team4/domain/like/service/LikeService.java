4cbebb5b63f61becbbb5842bc3af3dd91eff21dd
Dsys1129 <dsys1129@gmail.com>
2023-07-25 01:50:45 +0900

    fix: LikeService
    AccountInfo 조회 삭제
    post.updateLikeCount() 분리 적용

--- src/main/java/com/clone/team4/domain/like/service/LikeService.java
+++ src/main/java/com/clone/team4/domain/like/service/LikeService.java
@@ -6,11 +6,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.clone.team4.domain.like.entity.Like;
 import com.clone.team4.domain.like.repository.LikeRepository;
-import com.clone.team4.domain.like.repository.QLikeRepository;
 import com.clone.team4.domain.post.entity.Post;
 import com.clone.team4.domain.post.repository.PostRepository;
 import com.clone.team4.domain.user.entity.AccountInfo;
-import com.clone.team4.domain.user.repository.AccountInfoRepository;
 import com.clone.team4.global.dto.BaseResponseDto;
 
 import lombok.RequiredArgsConstructor;
@@ -21,8 +19,6 @@ public class LikeService {
 
     private final LikeRepository likeRepository;
     private final PostRepository postRepository;
-    private final AccountInfoRepository accountInfoRepository;
-    private final QLikeRepository qLikeRepository;
 
     @Transactional
     public BaseResponseDto postLike(Long postId, AccountInfo accountInfo) {
@@ -30,20 +26,15 @@ public class LikeService {
         Post findPost = postRepository.findById(postId)
             .orElseThrow(() -> new IllegalArgumentException("해당 포스트가 없습니다."));
 
-        AccountInfo findAccountInfo = accountInfoRepository.findById(accountInfo.getId())
-            .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
-
-        Like findLike = qLikeRepository.findByPostIdAndAccountId(findPost.getId(),
-            findAccountInfo.getId());
-
-        Long likeCount = findPost.getLikeCount();
+        Like findLike = likeRepository.findByPostIdAndAccountId(findPost.getId(),
+            accountInfo.getId());
 
         if (findLike != null) {
+            findPost.decreaseLikeCount();
             likeRepository.delete(findLike);
-            findPost.updateLikeCount(--likeCount);
         } else {
-            likeRepository.save(new Like(findAccountInfo, findPost));
-            findPost.updateLikeCount(++likeCount);
+            findPost.increaseLikeCount();
+            likeRepository.save(new Like(accountInfo, findPost));
         }
 
         BaseResponseDto response = BaseResponseDto.builder()
