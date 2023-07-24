42998a70fd70941b589b555e7b4901e9fb29b986
Dsys1129 <dsys1129@gmail.com>
2023-07-25 01:45:47 +0900

    fix: add QLikeRepositoryImpl

--- src/main/java/com/clone/team4/domain/like/repository/LikeRepository.java
+++ src/main/java/com/clone/team4/domain/like/repository/LikeRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.clone.team4.domain.like.entity.Like;
 
-public interface LikeRepository extends JpaRepository<Like, Long> {
+public interface LikeRepository extends JpaRepository<Like, Long>, QLikeRepository {
 }
