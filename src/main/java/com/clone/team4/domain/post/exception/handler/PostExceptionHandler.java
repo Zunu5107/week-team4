267ea0940c4565cde0dd602c5895a6e0ae161918
Zunu5107 <cosoob5107@gmail.com>
2023-07-26 01:24:15 +0900

    hotfix

--- src/main/java/com/clone/team4/domain/post/exception/handler/PostExceptionHandler.java
+++ src/main/java/com/clone/team4/domain/post/exception/handler/PostExceptionHandler.java
@@ -3,12 +3,15 @@ package com.clone.team4.domain.post.exception.handler;
 import com.clone.team4.domain.post.exception.PostNotFoundException;
 import com.clone.team4.global.dto.BaseResponseDto;
 import com.clone.team4.global.exception.PermissionDeniedException;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+import static org.slf4j.LoggerFactory.getLogger;
+
 public interface PostExceptionHandler {
 
     @ExceptionHandler({PostNotFoundException.class, IllegalArgumentException.class})
@@ -36,5 +39,12 @@ public interface PostExceptionHandler {
             .msg(e.getMessage())
             .build();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+    
+    private static Logger logger() {
+        final class LogHolder {
+            private static final Logger LOGGER = getLogger(PostExceptionHandler.class);
+        }
+        return LogHolder.LOGGER;
     }
 }
