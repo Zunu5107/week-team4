1e8007a298064e184fde84283a112df9ea39837b
Zunu5107 <cosoob5107@gmail.com>
2023-07-26 17:38:16 +0900

    v0.3.2 hotfix CustomStatusException

--- src/main/java/com/clone/team4/global/exception/CustomStatusException.java
+++ src/main/java/com/clone/team4/global/exception/CustomStatusException.java
@@ -24,6 +24,7 @@ public class CustomStatusException extends RuntimeException{
 
         public CustomStatusExceptionBuilder(String message) {
             this.result = new CustomStatusException(message);
+            this.result.status = HttpStatus.NOT_FOUND;
         }
 
         public CustomStatusExceptionBuilder status(int status){
