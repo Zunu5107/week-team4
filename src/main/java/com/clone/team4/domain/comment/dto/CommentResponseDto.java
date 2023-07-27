b511ef1d0df09708853e700a2d3e40d08e1125c3
kki4504 <rhkddlf9876@gmail.com>
2023-07-27 17:08:27 +0900

    fix: commentResponsDto

--- src/main/java/com/clone/team4/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/com/clone/team4/domain/comment/dto/CommentResponseDto.java
@@ -3,17 +3,21 @@ package com.clone.team4.domain.comment.dto;
 import com.clone.team4.domain.comment.entity.Comment;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class CommentResponseDto {
     private Long id;
     private String userImage;
     private String comment;
     private String nickname;
+    private LocalDateTime createdAt;
 
     public CommentResponseDto(Comment comment){
         this.id = comment.getId();
         this.userImage = comment.getAccountInfo().getProfileImage();
         this.comment = comment.getComment();
         this.nickname = comment.getAccountInfo().getNickname();
+        this.createdAt = comment.getCreatedAt();
     }
 }
